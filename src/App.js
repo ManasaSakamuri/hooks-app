85e48b01e2ad3cb1848b95634abfb47b36e9980d
manasasakamuri <sakamurimanasa7176@gmail.com>
2021-11-26 11:27:22 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import './App.css';
 import React from 'react';
 import ClickCounter from './components/higherorderComponents/ClickCounter';
 import HoverCounter from './components/higherorderComponents/HoverCounter';
+import DocTitle from './components/customhooks/effects/DocTitle';
+import DocTitle2 from './components/customhooks/effects/DocTitle2';
+import useDocTitle from './components/customhooks/effects/useDocTitle';
+import Counter1 from './components/customhooks/effects/functionality/Counter1';
+import Counter2 from './components/customhooks/effects/functionality/Counter2';
+import useCounter from './components/customhooks/effects/functionality/useCounter';
 
 function App() {
   return (
       <div className="App">
-        <HoverCounter/>
+        <useCounter/>
       </div>
     
   );
